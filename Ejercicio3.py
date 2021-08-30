8685fea983f618c2fc742bb88dd318a5ef00cf84
denisevaldes <cadelige@gmail.com>
2021-08-30 00:19:24 -0400

    update

---
+++ Ejercicio3.py
@@ -0,0 +1,4 @@
+#Una multitienda tiene una promoción: a todos los trajes que tienen un precio superior a $10000 se les
+#aplicará un descuento de 15 %, a todos los demás se les aplicará sólo 8 %. Realice un programa para
+#determinar el precio final que debe pagar una persona por comprar un traje y de cuánto es el descuento
+#que obtendrá. Pida por lo menos ingresar 3 elementos.
